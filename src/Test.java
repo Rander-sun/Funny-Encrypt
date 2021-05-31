8f9a1898c8b1af4a07080a1cd0a701fea025545c
Yang-Zhao <823504052@qq.com>
2021-05-31 14:03:56 +0800

    zy-init

---
+++ src/Test.java
@@ -0,0 +1,78 @@
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
+public class Test {
+
+   public static void main(String[] args) throws IOException {
+     Image image=new Image("E://大学//OOP//大作业//Image encryption//ima//1.png");
+     Util.jpg2png(image.getimage(),"E://大学//OOP//大作业//Image encryption//ima//1.png");
+     image=new Image("E://大学//OOP//大作业//Image encryption//ima//1.png");
+     Scanner in=new Scanner(System.in);
+     System.out.println("请输入需要加密的文字");
+     String content = in.nextLine();
+     System.out.println("请输入您设置的密码");
+     String password = in.nextLine();
+     Test.encrypt(image,content,password);
+     //解密
+     Image image1=new Image("E://大学//OOP//大作业//Image encryption//ima//1_.png");
+     System.out.println("请输入设置好的密码");
+     String password_=in.nextLine();
+     System.out.println("请输入生成的密码");
+     String uPassword=in.nextLine();
+     String deCodeContent=Test.deCode(image1,uPassword,password_);
+     System.out.println("解密后："+deCodeContent);
+   }
+
+   public static String encrypt(Image image,String content,String password) throws IOException {
+     int a=0,r=0,g=0,b=0;
+     String uPassword = " ";
+
+     System.out.println("加密前：" + content);
+     byte[] encode = AesEncrypt.aesEncrypt(content, password);
+
+     String code = Util.Byte2Hex(encode);
+     //System.out.println("密文字符串：" + code);
+     image.genCoordinate(image.getimage(), code.length()/4);
+     for(int i=0;i<code.length()-3;i=i+4){
+       a=code.charAt(i);
+       r=code.charAt(i+1);
+       g=code.charAt(i+2);
+       b=code.charAt(i+3);
+       Integer[] keys = image.coordinate.keySet().toArray(new Integer[0]);
+       Random random = new Random();
+       Integer x = keys[random.nextInt(keys.length)];
+       Integer y = image.coordinate.get(x);
+       uPassword=uPassword+image.setImagePixel(image.getimage(), a,r,g,b,x,y);
+       image.coordinate.remove(x);
+     }
+     ImageIO.write(image.getimage(), "png",  new File("E://大学//OOP//大作业//Image encryption//ima//1_.png"));
+     uPassword=Util.decodeUnicode(uPassword);
+     System.out.println("生成密码："+uPassword);
+     return uPassword;
+   }
+
+   public static String deCode(Image image1,String uPassword,String password)
+       throws UnsupportedEncodingException {
+     String recode="";
+     uPassword=Util.encodeUnicode(uPassword);
+     //System.out.println("解密坐标为"+uPassword);
+     String[] passBuf=uPassword.split("\\s+");
+     //System.out.println(passBuf);
+     for(int i=0;i<passBuf.length-1;i+=2){
+       int x=Integer.parseInt(passBuf[i]);
+       int y=Integer.parseInt(passBuf[i+1]);
+       int[] argb=image1.getImagePixel(image1.getimage(), x,y);
+       recode=recode+(char)argb[0]+(char)argb[1]+(char)argb[2]+(char)argb[3];
+     }
+     //System.out.println(recode);
+     byte[] decode = Util.Hex2Byte(recode);
+
+     byte[] decryptResult = AesEncrypt.decrypt(decode, password);
+     return new String(decryptResult, "UTF-8");
+   }
+}
---
+++ src/Util.java
@@ -0,0 +1,68 @@
+import java.awt.Graphics2D;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Util {
+  public static String Byte2Hex(byte buf[]) {
+    StringBuffer strBuf = new StringBuffer();
+    for (int i = 0; i < buf.length; i++) {
+      String hex = Integer.toHexString(buf[i] & 0xFF);
+      if (hex.length() == 1) {
+        hex = '0' + hex;
+      }
+      strBuf.append(hex.toUpperCase());
+    }
+    return strBuf.toString();
+  }
+
+  public static byte[] Hex2Byte(String hexStr) {
+    if (hexStr.length() < 1)
+      return null;
+    byte[] result = new byte[hexStr.length() / 2];
+    for (int i = 0; i < hexStr.length() / 2; i++) {
+      int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+      int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+      result[i] = (byte) (high * 16 + low);
+    }
+    return result;
+  }
+
+  public static String decodeUnicode(String unicode){
+
+    StringBuffer string = new StringBuffer();
+    String[] hex = unicode.split("\\s+");
+    for (int i = 1; i < hex.length; i++) {
+      int data = Integer.parseInt(hex[i], 16);
+      string.append((char) data);
+    }
+    return string.toString();
+  }
+
+  public static String encodeUnicode(String string){
+    StringBuffer unicode = new StringBuffer();
+    for (int i = 0; i < string.length(); i++) {
+      char c = string.charAt(i);
+      unicode.append(Integer.toHexString(c)+" ");
+    }
+    return unicode.toString();
+  }
+
+  public static void jpg2png(BufferedImage image,String outFile) throws IOException {
+
+    int width= image.getWidth();
+    int height= image.getHeight();
+    BufferedImage bufferedImage = new BufferedImage(width, height, image.getType());
+    Graphics2D gr = bufferedImage.createGraphics();
+    bufferedImage = gr.getDeviceConfiguration().createCompatibleImage(width, height, Transparency.TRANSLUCENT);
+
+    gr = bufferedImage.createGraphics();
+    gr.drawImage(image.getScaledInstance(width, height, image.SCALE_DEFAULT), 0, 0, width, height, null);
+    gr.dispose();
+
+    File file = new File(outFile);
+    ImageIO.write(bufferedImage, "png", file);
+  }
+}
